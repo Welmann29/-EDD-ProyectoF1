c7d0009980cbfff72886899abce93cd38a636a58
Evelyn Alejandra Navarro Ozorio <aleha16navarro@gmail.com>
2020-12-10 21:50:40 -0600

    Actualización BaseDatos

--- BaseDatos.py
+++ BaseDatos.py
@@ -7,50 +7,75 @@ class BaseDatos:
 
 
     # == BUSCAR BASES DE DATOS
-
     def Buscar(self, table):
-
-        for table_name in self.list_table:
-
-            if table_name.NAME == table:
-                return table_name
-        else:
-            return False
+        salida = []
+        existe = False
+        col = 0
+        i = 0
+        for nombre in self.list_table:
+            if nombre[0] == table:
+                existe = True
+                col = nombre[1]
+                break
+            else:
+                existe = False
+                i = i+1
+        salida = [existe, i, col]
+        return salida
 
 
     # == CREAR TABLAS
 
     def createTable(self, tableName, numberColumns):
-
-        temp = self.Buscar(tableName)
-
-        if not temp:
-            print("Creando tabla: "+tableName)
-            #self.list_table.append(Tabla(tableName, numberColumns))
-            pass
+        salida = []
+        if len(self.list_table) == 0:
+            self.list_table.append([tableName, numberColumns])
+            existe = True
         else:
-            pass
-
-    # MOSTRAR TABLAS
+            salida = self.Buscar(tableName)
+            existe = salida[0]
+            if existe == False:
+                self.list_table.append([tableName, numberColumns])
+            
+    # == MOSTRAR TABLAS
     def showTables(self):
+        nombre = []
+        for n in self.list_table:
+            nombre.append(n[0])
+        print(nombre)
 
-        print("//==============================//")
-        print(" - -   Tablas BD:    - -")
-        for tables in self.list_table:
-            print(tables.Name)
-        print("//==============================//")
-
-
-
-
-    # === alterTable
-
-    # === dropTable
+    # == CAMBIAR NOMBRES
+    def alterTable(self, tableOld, tableNew):
+        salida = self.Buscar(tableOld)
+        if salida[0]:
+            self.list_table[salida[1]][0]= tableNew
+        else:
+            return 1
+    
+    # === ELIMINAR TABLA
+    def dropTable(self,tableName):
+        salida = self.Buscar(tableName)
+        if salida[0]:
+            self.list_table.pop(salida[1])
+        else:
+            return 1
 
     # === alterAdd
 
     # === alterDrop
 
-    # === extractTable
+    # === extractTable 276
 
-
+B = BaseDatos('Alejandra')
+B.createTable('Ale',10)
+B.createTable('Marcos',5)
+B.createTable('Alejandro',20)
+B.createTable('Ale',1)
+B.createTable('Navarro',7)
+B.showTables()
+B.dropTable('Ale')
+B.showTables()
+B.createTable('YaFeliz:)',7)
+B.showTables()
+B.alterTable('Navarro','AleFeliz')
+B.showTables()
